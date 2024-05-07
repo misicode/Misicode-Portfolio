703f3bb7284e619e70181ae063b8787a681b2597
misicode <alee_1225@hotmail.com>
2024-05-06 22:54:23 -0500

    feat: update about-me component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,13 @@ module.exports = {
       colors: {
         primary: {
           500: "#EE2CFF",
+          600: "#EC00FE",
+          700: "#D900EA",
         },
         secondary: {
-          700: "#120B3B",
-          800: "#0E092D",
-          900: "#0A0521",
+          700: "#170E3A",
+          800: "#120B2B",
+          900: "#0D081E",
         },
       },
       fontFamily: {
